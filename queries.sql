c33d8f5eb61c107a0e67bbc6972787cf1baf0529
Andi Ernhofer <aernhofer@student.tgm.ac.at>
2015-03-11 20:21:28 +0100

    selects teilweise

--- queries.sql
+++ queries.sql
@@ -1,21 +1,28 @@
 --1. Geben Sie alle Mannschaften aus, die bei der Bodenseeregatta im Jahr 2014 teilgenommen haben. Wenn eine Mannschaft mit einem Boot mit der Segelfläche kleiner als 20 m2 teilgenommen hat, soll auch die ID des Bootes ausgegeben werden.
+SELECT mname AS mannschaft, (SELECT id FROM sportboot WHERE segelflaeche<20 AND id=sportboot) AS boot_id FROM nimmt_teil WHERE rname = 'Bodenseeregatta' AND rjahr = 2014;
 
 --2. Geben Sie den Namen und das Geburtsdatum der jüngsten Trainer aus (können auch mehrere sein).
 SELECT name,geburtsdatum FROM person ORDER BY geburtsdatum DESC LIMIT 25;
 
 --3. Geben Sie alle Personen geordnet nach Geburtsdatum aus, die sowohl Segler als auch Trainer sind, allerdings in keiner Mannschaft dabei sind.
+SELECT name,key,geburtsdatum FROM person natural join trainer natural join segler WHERE key NOT IN (SELECT key FROM mannschaft) AND key NOT IN (SELECT key FROM bildet) ORDER BY geburtsdatum ASC;
 
 --4. Geben Sie alle Personen geordnet nach Geburtsdatum aus, die entweder Segler oder Trainer sind, jedoch nicht beides und vermerken Sie in einer Spalte, ob es sich um einen Trainer oder einen Segler handelt.
 
 --5. Geben Sie die Regatten (Name und Jahr) mit den wenigsten Wettfahrten an und geben Sie auch die Anzahl aus.
+SELECT name,jahr,COUNT(datum) AS anzahl FROM wettfahrt GROUP BY name,jahr;
 
 --6. Geben Sie die Namen jener Trainer aus, die zwei oder mehr Mannschaften betreuen.
+SELECT name,mannschaften FROM (SELECT name, (SELECT COUNT(name) FROM mannschaft WHERE key = person.key) AS mannschaften FROM person NATURAL JOIN trainer) AS trainer where mannschaften >2;
 
 --7. Welche Altersklasse ist am aktivsten (hat an den meisten Wettfahrten Punkte erzielt)?
+SELECT altersklasse FROM (SELECT aklasse AS altersklasse,sum(punkte) AS punkte FROM erzielt LEFT JOIN mannschaft ON erzielt.mname=mannschaft.name GROUP BY aklasse ORDER BY punkte DESC) AS altersklassen LIMIT 1;
 
 --8. Um wieviel gehen Tourenboote durchschnittlich tiefer als Sportboote?
+SELECT AVG(tiefgang)-(SELECT AVG(tiefgang) FROM sportboot NATURAL JOIN boot) as differenz FROM tourenboot NATURAL JOIN boot;
 
 --9. Geben Sie für alle Mannschaften aus, an wievielen Regatten sie bereits teilgenommen haben und wieviele Punkte sie dort erzielt haben.
+SELECT mname AS mannschaft,COUNT(mname) AS anzahl, sum(punkte) AS punkte FROM (SELECT mname,wname,sum(punkte) as punkte FROM erzielt GROUP BY mname,wname) AS teilnehmen GROUP BY mname;
 
 --10. Welches Land bietet die längste Wettfahrtsstrecke und hat zusätzlich nicht die kürzeste?
 
